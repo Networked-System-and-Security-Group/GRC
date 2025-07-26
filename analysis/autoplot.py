24b7ff525cd483e353dbd2951db0484e0e887299
Zhongguuu <zihanniu85@gmail.com>
2025-07-26 16:19:34 +0000

    完成部分自动绘图脚本

--- analysis/autoplot.py
+++ analysis/autoplot.py
@@ -6,7 +6,40 @@ def main():
     # 负责绘制
     parser = argparse.ArgumentParser()
     parser.add_argument('-e', '--expr', type=str)
-    parser.add_argument('-n', '--name', type=str)
+    parser.add_argument('-f', '--flow_type', type=str)
+
+    args = parser.parse_args()
+    flow_type = args.flow_type
+    expr = args.expr
+    
+    # 首先绘制AVG FCT
+
+    dcqcn = []
+    dcqcn_ecn = []
+    gscc_intra = []
+    gscc_inter = []
+    gscc_inter_large = []
+    gscc_inter_small = []
+    
+    wan_cc_mode = 0
+    for ana in analyser_iter(expr):
+        try:
+            avg_vals, avg_intra, avg_inter = ana.get_avg_fct()
+            p99_vals, p99_intra, p99_inter = ana.get_p99_fct()
+            # results[wan_cc_mode].append({
+            #     'ID': ana.id,
+            #     'Avg_FCT': avg_vals,
+            #     'Avg_Intra_FCT': avg_intra,
+            #     'Avg_Inter_FCT': avg_inter,
+            #     'P99_FCT': p99_vals,
+            #     'P99_Intra_FCT': p99_intra,
+            #     'P99_Inter_FCT': p99_inter
+            # })
+        except Exception as e:
+            print(f'Error processing {ana.id}: {e}')
+        
+        wan_cc_mode = (wan_cc_mode + 1) % 3
+
     
     return
 
